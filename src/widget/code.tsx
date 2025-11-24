d98064a2e4aa3b107bf811ec9f57e555c9909fb8
Myro <myroslav.galavai@gmail.com>
2025-11-24 09:22:03 +0100

    Fix badge cropping - increase height to 90px
    - Increase badge height from 75px to 90px to prevent top/right cropping
    - Ensures full badge with stitched borders is visible

--- src/widget/code.tsx
+++ src/widget/code.tsx
@@ -1446,7 +1446,7 @@ function AdventCalendar() {
             <WidgetImage
                 src={ADVENT_CALENDAR_BADGE_URL}
                 width={262}
-                height={75}
+                height={90}
             />
 
             {/* Calendar Grid */}
